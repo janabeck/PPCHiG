32f351b42bad5b69bb782c80f7da65b8d3d05703
Jana Beck <janabeck@babel.ling.upenn.edu>
2012-01-10 18:08:04 -0500

    added directory for Annotald validation scripts and moved check-CODE.py there

---
+++ annotald/validation-scripts/check-CODE.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# make sure all CODE items (VS, COM, MAN, TODO) only have CODE as parent
+
+import re
+
+import os.path
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+import sys
+sys.path.append(SCRIPT_DIR + "/../../../../../myGit/TreeTransformer")
+
+from lovett.annotald import flagIf
+from lovett.searchfns import *
+
+code_stuff = re.compile("{VS:.*|{COM:.*|{MAN:.*|{TODO:.*")
+
+flagIf(hasLeafLabel(code_stuff) & ~hasLabel("CODE"))
+
