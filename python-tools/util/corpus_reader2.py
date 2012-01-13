a166556b5b68cfd2c3b575f71629145fd20dd84d
Jana Beck <janabeck@babel.ling.upenn.edu>
2012-01-13 01:30:33 -0500

    made many modifications to CR2: can now add continuity milestones, renumber/add IDs. CR2 also now can determine what format a corpus is in and will parse tokens accordingly (although the 'deep' format is not yet supported).

--- python-tools/util/corpus_reader2.py
+++ python-tools/util/corpus_reader2.py
@@ -17,6 +17,7 @@ import nltk.tree as T
 
 class Token():
     """A class for Penn-style parsed trees."""
+    #TODO: represent lemmas in the data structure
     
     def __init__(self, str):
         """Initialize a token."""
@@ -24,25 +25,30 @@ class Token():
         # _tree contains the token as an nltk Tree
         self._tree = T.ParentedTree(str)
         
-        # id contains the token's ID as a string "Corpus,Book.x"
+        # id contains the token's ID as a string "Corpus,Book:[milestones].num
         self.id = ""
 
+        # id_num contains the numerical index of the ID as an integer
+        self.id_num = 0
+
+        # id_tree contains the ID subtree
+        self.id_tree = ""
+        
         # corpus contains the corpus part of the ID as a string
         self.corpus = ""
 
         # book contains the book (= filename) part of the ID as a string
         self.book = ""
 
-        # id_num contains the numerical index of the ID as an integer
-        self.id_num = 0
-
         # milestones contains all the milestones in a particular token
         self.milestones = []
         
         # words contains all the words in a sentence token, exclusive of punctuation, as a list
+        # excludes lemmas, if dash format
         self.words = []
 
         # text contains all the words in a sentence token, including punctuation, as a list
+        # excludes lemmas, if dash format
         self.text = []
             
         # pos contains all the words in a sentence token with no punctuation or empty categories as a list of tuples
@@ -61,25 +67,30 @@ class Token():
         # root contains the top-most phrase structure label, e.g., IP-MAT
         self.root = ""
 
+        # main_tree contains the root subtree
+        self.main_tree = ""
+
         # metadata contains the METADATA sub-tree if present
         self.metadata = ""
 
-    def parse(self, tree):
+    def parse(self, tree, format):
         """Fill Token data structure."""
         
         # finds METADATA node, stores it, and then removes it so as not to interfere with word count, etc.
         for tr in tree.subtrees():
             if tr.node == "METADATA":
                 self.metadata = tr
-                main_tree = tr.right_sibling
-                if main_tree.right_sibling:
-                    id_tree = main_tree.right_sibling
-                    tree = T.Tree("", [main_tree, id_tree])
+                self.main_tree = tr.right_sibling
+                if self.main_tree.right_sibling:
+                    self.id_tree = self.main_tree.right_sibling
+                    tree = T.Tree("", [self.main_tree, self.id_tree])
                     tree = T.ParentedTree.convert(tree)
                 else:
-                    tree = T.Tree("", [main_tree])
+                    tree = T.Tree("", [self.main_tree])
 
         id_str = re.compile("^(.*),(.*):[0-9_;a-z]*\.([0-9]*)$")
+
+        alt_id_str = re.compile("^(.*),(.*)\.([0-9]*)$")
 
         # finds $ROOT and stores in self.root
 
@@ -88,9 +99,11 @@ class Token():
         for tr in tree.subtrees():
             subtrs.append(tr)
 
-        root = subtrs[1]
+        self.main_tree = subtrs[1]
 
-        self.root = root.node
+        self.root = self.main_tree.node
+
+        ortho_lemma = re.compile("(.*)-(.*)")
         
         # gathers remaining info        
         for tup in tree.pos():
@@ -106,10 +119,16 @@ class Token():
                 self.todos.append(leaf)                
             elif tag.find("ID") != -1:
                 self.id = leaf
-                id_stuff = id_str.match(leaf)
-                self.corpus = id_stuff.group(1)
-                self.book = id_stuff.group(2)
-                self.id_num = id_stuff.group(3)
+                try:
+                    id_stuff = id_str.match(leaf)
+                    self.corpus = id_stuff.group(1)
+                    self.book = id_stuff.group(2)
+                    self.id_num = id_stuff.group(3)
+                except AttributeError:
+                    id_stuff = alt_id_str.match(leaf)
+                    self.corpus = id_stuff.group(1)
+                    self.book = id_stuff.group(2)
+                    self.id_num = id_stuff.group(3)                    
             # catches punctuation. allowed punctuation POS tags are , . "
             elif tag.find(",") != -1 or tag.find(".") != -1 or tag.find("\"") != -1:
                 self.text.append(leaf)
@@ -124,9 +143,28 @@ class Token():
                 pass
             # catches everything else = just words
             else:
-                self.text.append(leaf)
-                self.pos.append((leaf, tag))
-                self.words.append(leaf)
+                if format == "old":
+                    self.text.append(leaf)
+                    self.pos.append((leaf, tag))
+                    self.words.append(leaf)
+                elif format == "dash":
+                    match = ortho_lemma.match(leaf)
+                    ortho = match.group(1)
+                    lemma = match.group(2)
+                    self.text.append(ortho)
+                    self.pos.append(((ortho, lemma), tag))
+                    self.words.append(ortho)
+
+    def has_milestone_first(self):
+        """Check to see that the tree starts with a (CODE {VS:...}) milestone."""
+
+        tree = self.main_tree
+        pos_list = tree.pos()
+        if pos_list[0][0].find("VS:") == -1 and pos_list[0][1] != "FORMAT":
+            self._tree.pprint()
+            return False
+
+        return True
 
 class Corpus():
     """A class for a database of Penn-style parsed trees."""
@@ -136,6 +174,9 @@ class Corpus():
         # trees contains keys = numerical indices corresponding to sequence of tokens in file and values = Token instances
         self.tokens = {}
 
+        #TODO: add a variable for what format the corpus is in: old (default), dash, deep
+        self.format = ""
+
     def load(self, trees):
         """Initializes Token objects and fills corpus instance."""
 
@@ -143,10 +184,33 @@ class Corpus():
 
         for tree in trees:
             tok = Token(tree)
-            tok.parse(tok._tree)
+            if count == 0:
+                if tree.find("VERSION") != -1:
+                    self.parse_version(tok._tree)
+                else:
+                    print
+                    print "This corpus file is in the old format."
+                    print
+                    self.format = "old"
+            tok.parse(tok._tree, self.format)
             self.tokens[count] = tok
             count += 1
 
+    def parse_version(self, tree):
+        """Record what format the corpus file is in."""
+
+        pos_list = tree.pos()
+        if pos_list[0][0] == "dash":
+            print
+            print "This corpus file is in the 'dash' format."
+            print
+            self.format = "dash"
+        elif pos_list[0][0] == "deep":
+            print
+            print "This corpus file is in the 'deep' format."
+            print
+            self.format = "deep"
+            
     def check_for_ids(self, output):
         """Check to see that all trees have ID nodes."""
         
@@ -188,17 +252,18 @@ class Corpus():
 
         return True
 
+    def check_milestones(self):
+        """Check to see that every token starts with a (CODE {VS:...}) milestone."""
+
+        for key in self.tokens.keys():
+            tree = self.tokens[key]
+            if not tree.has_milestone_first():
+                return False
+
+        return True
+
     def word_count(self):
         """Count all and only the words in the .psd file."""
-
-        if self.check_for_ids(False):
-            print "# # # # # # # # # # # # # # # #"
-            print
-            print "All sentences have IDs!"
-
-        if self.check_seq_ids():
-            print
-            print "All IDs in sequence!"
 
         word_count = 0
 
@@ -208,27 +273,113 @@ class Corpus():
             tok = self.tokens[key]
             for word in tok.words:
                 word_count += 1
-                
+        
+        print "# # # # # # # # # # # # # # # #"        
         print
         print "There are " + str(word_count) + " words in this file, excluding empty categories and punctuation."
-        print
-        print "# # # # # # # # # # # # # # # #"
+
+        if self.check_for_ids(False):
+            print
+            print "All sentences have IDs!"
+
+        if self.check_seq_ids():
+            print
+            print "All IDs in sequence!"
+            print
+            print "# # # # # # # # # # # # # # # #"
 
     def renumber_ids(self, filename):
         """Renumber/add IDs in the .psd file."""
 
-        pass
+        if not self.check_milestones():
+            print
+            print "Please run Corpus Reader with the -m option to add continuity \
+milestones before you renumber and/or add ID nodes!"
+            print
+            sys.exit()
+        else:
+            milestone = re.compile("{VS:([0-9]+_[0-9]+[a-z]*)}")
+            count = 1
+            corp = ""
+            bk = ""
+            for key in self.tokens.keys():
+                tree = self.tokens[key]
+                num_milestones = len(tree.milestones)
+                ms_lst = ""
+                for ms in tree.milestones:
+                    match = milestone.match(ms)
+                    ms_lst = ms_lst + match.group(1) + ";"
+                if tree.root != "VERSION":
+                    if count == 1:
+                        if tree.corpus == "" or tree.book == "":
+                            tree.corpus = raw_input("What is the name of your corpus? ")
+                            corp = tree.corpus
+                            tree.book = raw_input("What is the book (= filename with the extension)? ")
+                            bk = tree.book
+                        else:
+                            corp = tree.corpus
+                            bk = tree.book
+                    ## print "Old ID was " + tree.id
+                    ## print
+                    tree.id = corp + "," + bk + ":" + ms_lst[:-1] + "." + str(count)
+                    tree.id_num = count
+                    tree.id_tree = T.Tree("ID", [tree.id])
+                    new_tree = T.Tree("", [tree.metadata, tree.main_tree, tree.id_tree])
+                    tree._tree = T.ParentedTree.convert(new_tree)
+                    ## print "New ID is " + tree.id
+                    ## print
+                    count += 1
 
+            self.print_trees(filename)
+                
     def add_milestones(self, filename):
         """Add continuity milestones in the .psd file."""
 
-        lst_milestone = ""
+        milestone = re.compile("{VS:([0-9]+_[0-9]+)([a-z]*)}")
+
+        if not self.check_milestones():
+            lst_milestone = ""
+            lst_letter = 96
+            for key in self.tokens.keys():
+                tree = self.tokens[key]
+                num_milestones = len(tree.milestones)
+                # when milestones in the tree
+                try:
+                    match = milestone.match(tree.milestones[num_milestones - 1])
+                    if not tree.has_milestone_first():
+                        if lst_letter != 97:
+                            lst_letter += 1
+                        leaf = T.ParentedTree("CODE", ["{VS:" + lst_milestone + chr(lst_letter) + "}"])
+                        tree.milestones.insert(0, "{VS:" + lst_milestone + chr(lst_letter) + "}")
+                        # insert milestone as first leaf of main tree
+                        tree.main_tree.insert(0, leaf)
+                        # rebuild tree with wrapper
+                        new_tree = T.Tree("", [tree.metadata, tree.main_tree, tree.id_tree])
+                        tree._tree = T.ParentedTree.convert(new_tree)
+                    lst_milestone = match.group(1)
+                    try:
+                        lst_letter = ord(match.group(2))
+                    except TypeError:
+                        lst_letter = 96
+                # when no milestones in the tree
+                except IndexError:
+                    if tree.root != "VERSION":
+                        lst_letter += 1
+                        leaf = T.ParentedTree("CODE", ["{VS:" + lst_milestone + chr(lst_letter) + "}"])
+                        tree.milestones.append(lst_milestone)
+                        # insert milestone as first leaf of main tree
+                        tree.main_tree.insert(0, leaf)
+                        # rebuild tree with wrapper
+                        new_tree = T.Tree("", [tree.metadata, tree.main_tree, tree.id_tree])
+                        tree._tree = T.ParentedTree.convert(new_tree)
+                        
+            self.print_trees(filename)
 
     def print_trees(self, filename):
         """Just print the trees from an input file."""
         # (i.e., remove CS comment blocks from .out files)
 
-        out_name = filename.replace(".out",".out.new")
+        out_name = filename + ".new"
 
         out_file = open(out_name, "w")
 
@@ -344,7 +495,7 @@ def read(filename, out):
 def select(corpus):
     """Select another CR function from a menu."""
 
-    #TODO
+    #TODO: write the selector for less common functions!
     pass
 
 if __name__ == "__main__":
