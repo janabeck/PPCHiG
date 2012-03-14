a3ff8d67c2da97f68f7204a234369cfb5dc6ed2c
Jana Beck <janabeck@babel.ling.upenn.edu>
2012-03-14 11:52:45 -0400

    fixed transform_case() to cover compound words

--- python-tools/util/corpus_reader.py
+++ python-tools/util/corpus_reader.py
@@ -818,22 +818,41 @@ milestones before you renumber and/or add ID nodes!"
 
         cats = [adj, be, clpro, clq, det, noun, prnoun, other, pro, quant, vb, wadj, wd, wpro]
 
+        dashes = ["NOM","GEN","ACC","DAT"]
+
         for key in self.tokens.keys():
             tree = self.tokens[key]
             leaves = tree._tree.leaves()
             # get all the subtrees at the POS level
             for tr in tree._tree.subtrees():
                 verb = False
+                reflexive = False
+                compound = False
                 remainder = ""
+                no_change = False
                 if tr[0] in leaves:
                     word = tr[0]
                     tag = tr.node
+                    for dash in dashes:
+                        if tag.find(dash) != -1:
+                            no_change = True
                     ind_match = self.re_index.match(tag)
                     if ind_match:
                         index = ind_match.group(2)
                         tag = tag.replace(index, "")
                     else:
                         index = ""
+                    if "+" in tag and "SLF" in tag:
+                        comp_group = tag.partition("+")
+                        tag = comp_group[0]
+                        remainder = "+" + comp_group[2]
+                        reflexive = True
+                        compound = True
+                    elif "+" in tag and "NEG" in tag:
+                        comp_group = tag.partition("+")
+                        tag = comp_group[2]
+                        remainder = comp_group[0] + "+"
+                        compound = True
                     if participle.match(tag):
                         verb = True
                         verb_group = tag.partition("-")
@@ -853,11 +872,17 @@ milestones before you renumber and/or add ID nodes!"
                             else:
                                 coretag = tag
                                 dash = "NOM"
-                            if verb:
-                                tag = "-".join([coretag, remainder, dash])
-                            else:
-                                tag = "-".join([coretag, dash])
-                            tree.change_POS(tag, "", index, tr)
+                            if not no_change:
+                                if verb:
+                                    tag = "-".join([coretag, remainder, dash])
+                                elif compound:
+                                    if reflexive:
+                                        tag = "".join([coretag, remainder, "-" + dash])
+                                    else:
+                                        tag = "".join([remainder, coretag, "-" + dash])
+                                else:
+                                    tag = "-".join([coretag, dash])
+                                tree.change_POS(tag, "", index, tr)
 
         self.print_trees(filename)
 
