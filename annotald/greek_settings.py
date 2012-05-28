a2b28d96ec07935b9750f9e25c76e9cf4324700d
Jana Beck <janabeck@babel.ling.upenn.edu>
2012-05-28 16:30:45 -0400

    added some sanity-check validation queries

--- annotald/greek_settings.py
+++ annotald/greek_settings.py
@@ -6,6 +6,17 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 import sys
 sys.path.append(current_dir + "/../../../../Git/Academic/TreeTransformer")
 
+from lovett.annotald import flagIf, stdinValidator
+from lovett.searchfns import *
+from collections import OrderedDict
+
+import runpy
+
+vs = runpy.run_path(current_dir + "/../../../../Git/Academic/PPCHiG/annotald/validation-scripts/validation_settings.py")
+
+import re
+r = re.compile
+
 # List of paths to javascript files which should be included in the
 # Annotald UI
 extraJavascripts=["/Users/janabeck/Downloads/waxeye-0.8.0/src/javascript/waxeye.js",
@@ -21,10 +32,24 @@ colorCSS = True
 
 colorCSSPath = current_dir + "/color.css"
 
-from lovett.annotald import stdinValidator, flagIf
-from collections import OrderedDict
+# flags CPs that don't have a C
+CPwithoutC = flagIf(hasLabel("CP") & ~hasDaughter(hasLabel("C", True)) & ~isTrace() & ~hasDaughter(hasLabel("CONJP")))
+
+# flags finite IPs without a subject
+missingSBJ = flagIf(hasLabel(vs['subj_ips']) & ~hasDaughter(hasLabel(vs['subject'])) & ~hasDaughter(hasLabel("CONJP")) & ~hasParent(hasLabel("CP-CMP")))
+
+# flags IP-MAT, IP-SUB, or IP-IMP with a non-coindexed infinitive on its spine
+spineINF = flagIf(hasLabel(r("IP-MAT|IP-SUB|IP-IMP")) & hasDaughter(hasLabel(r("BEN.*|VBN.*"))) & ~hasDaughter(hasLabel(vs['finite'])))
+
+# flags IPs with multiple subjects
+multipleSBJ = flagIf(hasLabel("IP") & hasDaughter(hasLabel(vs['subject']) & hasSister(hasLabel(vs['subject']))))
+
 validators = OrderedDict([
     ("v1", stdinValidator("/Users/janabeck/Git/Academic/PPCHiG/annotald/validation-scripts/v1.py")),
     ("v2", stdinValidator("/Users/janabeck/Git/Academic/PPCHiG/annotald/validation-scripts/v2.py")),
-    ("sanity", stdinValidator("/Users/janabeck/Git/Academic/PPCHiG/annotald/validation-scripts/sanity.py"))
+    ("sanity", stdinValidator("/Users/janabeck/Git/Academic/PPCHiG/annotald/validation-scripts/sanity.py")),
+    ("CPs without C", CPwithoutC),
+    ("Missing subject", missingSBJ),
+    ("Infinitive on a finite spine", spineINF),
+    ("Multiple subjects", multipleSBJ)
 ])
