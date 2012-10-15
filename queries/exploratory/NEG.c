c7496ea4230b367dc0050f3bd301a2c28437ac96
Jana Beck <janabeck@babel.ling.upenn.edu>
2012-10-14 20:57:33 -0400

    modified NEG.c to look for whether subject is overt or not, updated results

--- queries/exploratory/NEG.c
+++ queries/exploratory/NEG.c
@@ -1,4 +1,4 @@
-add_to_ignore: RPAREN|LPAREN|\**
+add_to_ignore: RPAREN|LPAREN
 
 node: IP*
 
@@ -8,6 +8,7 @@ a = ambiguous because both first position and pre-verbal
 f = unambiguously first position
 c = ambiguous because both second position and pre-verbal
 v = unambiguously pre-verbal
+p = unambiguously pre-verbal, *pro* subject
 s = unambiguously second position
 o = other
 e = no negation
@@ -15,11 +16,15 @@ e = no negation
 
 coding_query:
 
-1: { l: (IP-MAT*|IP-SUB* iDomsFirst NEG) AND (IP-MAT*|IP-SUB* domsWords 1) AND (NEG iDoms !*@*)
+1: { p: (IP-MAT*|IP-SUB* iDoms NP-SBJ*) AND (NP-SBJ* iDoms \*pro\*|\*con\*)
+     o: ELSE }
+
+2: { l: (IP-MAT*|IP-SUB* iDomsFirst NEG) AND (IP-MAT*|IP-SUB* domsWords 1) AND (NEG iDoms !*@*)
      a: (IP-MAT*|IP-SUB* iDomsFirst NEG) AND (NEG iPrecedes VB*|BE*) AND (NEG iDoms !*@*)
      f: (IP-MAT*|IP-SUB* iDomsFirst NEG) AND (NEG iDoms !*@*)
      c: (IP-MAT*|IP-SUB* iDoms VB*|BE*) AND (VB*|BE* iDoms !\*ICH\**) AND (NEG iPrecedes VB*|BE*) AND (NEG iDoms !*@*) AND (IP-MAT*|IP-SUB* iDomsNumber 2 NEG)
-     v: (IP-MAT*|IP-SUB* iDoms VB*|BE*) AND (VB*|BE* iDoms !\*ICH\**) AND (NEG iPrecedes VB*|BE*) AND (NEG iDoms !*@*)
+     v: (IP-MAT*|IP-SUB* iDoms VB*|BE*) AND (IP-MAT*|IP-SUB* iDoms NEG) AND (VB*|BE* iDoms !\*ICH\**) AND (NEG iPrecedes VB*|BE*) AND (NEG iDoms !*@*)
+     p: (IP-MAT*|IP-SUB* iDoms VB*|BE*) AND (IP-MAT*|IP-SUB* iDoms NEG) AND (VB*|BE* iDoms !\*ICH\**) AND (NEG iPrecedes NP-SBJ*) AND (NP-SBJ* iDoms \*pro\*|\*con\*) AND (NEG iDoms !*@*) AND (NP-SBJ* iPrecedes VB*|BE*)
      s: (IP-MAT*|IP-SUB* iDomsNumber 2 NEG) AND (NEG iDoms !*@*)
      o: (IP-MAT*|IP-SUB* iDoms NEG) AND (NEG iDoms !*@*)
      e: ELSE }
