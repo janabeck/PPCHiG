5164d4c54d3f0a1075c71dc5f448c06419a36a2f
Jana Beck <janabeck@babel.ling.upenn.edu>
2013-03-22 13:17:54 -0400

    added calculation (and rounding) of all percentages

--- python-tools/Perseus/label_coding_strings.py
+++ python-tools/Perseus/label_coding_strings.py
@@ -12,6 +12,10 @@ def main():
 				'v:e:n:-:o:-:f:f:t': ['OV$', 'OV', 'verb-final'],
 				'v:e:n:-:v:-:f:f:f': ['xVOx', 'VO', 'verb-medial'],
 				'v:e:n:-:v:-:f:t:f': ['^VOx', 'VO', 'verb-initial'],
+				'v:e:n:s:o:o:f:f:f': ['xOVx', 'OV', 'verb-medial'],
+				'v:e:n:s:o:o:f:f:t': ['OV$', 'OV', 'verb-final'],
+				'v:e:n:s:v:s:f:f:f': ['xVOx', 'VO', 'verb-medial'],
+				'v:e:n:v:v:o:f:t:f': ['^VOx', 'VO', 'verb-initial'],
 				'v:n:n:s:o:o:f:f:f': ['OSVx', 'OV', 'verb-medial'],
 				'v:n:n:s:o:o:f:f:t': ['OSV$', 'OV', 'verb-final'],
 				'v:n:n:s:o:s:f:f:f': ['SOVx', 'OV', 'verb-medial'],
@@ -79,39 +83,39 @@ def main():
 
 	v3 = sorted(verb_final, key=verb_final.__getitem__, reverse=True)
 
-	print >> out_file, "OV Sentences: " + str(ov) + " (" + str(((ov/total1) * 100)) + "%)"
+	print >> out_file, "OV Sentences: " + str(ov) + " (" + str(round(((ov/total1) * 100), 2)) + "%)"
 	print >> out_file
 
 	for i in s1:
-		print >> out_file, "\t" + i + "\t" + str(ov_pairs[i])
+		print >> out_file, "\t" + i + "\t" + str(ov_pairs[i]) + " (" + str(round(((ov_pairs[i]/ov)*100),2)) + "%)"
 
 	print >> out_file
-	print >> out_file, "VO Sentences: " + str(vo) + " (" + str((vo/total1) * 100) + "%)"
+	print >> out_file, "VO Sentences: " + str(vo) + " (" + str(round(((vo/total1) * 100),2)) + "%)"
 	print >> out_file
 
 	for i in s2:
-		print >> out_file, "\t" + i + "\t" + str(vo_pairs[i])
+		print >> out_file, "\t" + i + "\t" + str(vo_pairs[i]) + " (" + str(round(((vo_pairs[i]/vo)*100),2)) + "%)"
 
 	print >> out_file
-	print >> out_file, "Verb-initial Sentences: " + str(vi) + " (" + str((vi/total2) * 100) + "%)"
+	print >> out_file, "Verb-initial Sentences: " + str(vi) + " (" + str(round(((vi/total2) * 100),2)) + "%)"
 	print >> out_file
 
 	for i in v1:
-		print >> out_file, "\t" + i + "\t" + str(verb_initial[i])
+		print >> out_file, "\t" + i + "\t" + str(verb_initial[i]) + " (" + str(round(((verb_initial[i]/vi)*100),2)) + "%)"
 
 	print >> out_file
-	print >> out_file, "Verb-medial Sentences: " + str(vm) + " (" + str((vm/total2) * 100) + "%)"
+	print >> out_file, "Verb-medial Sentences: " + str(vm) + " (" + str(round(((vm/total2) * 100),2)) + "%)"
 	print >> out_file
 
 	for i in v2:
-		print >> out_file, "\t" + i + "\t" + str(verb_medial[i])
+		print >> out_file, "\t" + i + "\t" + str(verb_medial[i]) + " (" + str(round(((verb_medial[i]/vm)*100),2)) + "%)"
 
 	print >> out_file
-	print >> out_file, "Verb-final Sentences: " + str(vf) + " (" + str((vf/total2) * 100) + "%)"
+	print >> out_file, "Verb-final Sentences: " + str(vf) + " (" + str(round(((vf/total2) * 100),2)) + "%)"
 	print >> out_file
 
 	for i in v3:
-		print >> out_file, "\t" + i + "\t" + str(verb_final[i])
+		print >> out_file, "\t" + i + "\t" + str(verb_final[i]) + " (" + str(round(((verb_final[i]/vf)*100),2)) + "%)"
 
 	print >> out_file
 
